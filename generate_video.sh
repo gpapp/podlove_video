9ab24c837473a4970662f33d3e342bbf04bd9368
root <gergely.papp@itworks.hu>
2023-11-26 12:43:37 +0100

    Fixing video chapters

--- generate_video.sh
+++ generate_video.sh
@@ -6,6 +6,10 @@ PODCAST_TITLE="Zártosztály Podcast"
 CDN_BASE_URL="https://cdn.zartosztaly.hu/podcast"
 FONT="-font Liberation-Sans"
 
+function escape_ffmpeg_metadata (){
+  echo -e "$1" |sed -nE 's/([=;#\\])/\\\0/g;H;1h;${g;s/\n/\\n/g;p}'
+}
+
 function export_from_mysql () {
     source .env
     if [ -z $"SSH_CMD" ] || [ -z $"MYSQL_DB" ] || [ -z "MYSQL_USER" ] || [ -z "MYSQL_PASSWORD" ]|| [ -z "MYSQL_PREFIX" ]; then
@@ -21,7 +25,7 @@ select ep.number, ep.slug, ep.title, date_format(ep.recording_date,"%Y-%m-%dT%H:
         left join ${MYSQL_PREFIX}posts po on ep.post_id=po.id and po.post_status not in ("inherit", "auto-draft")
 	left join (select sep.id id, group_concat(concat(se.title,": ",se.original_url) SEPARATOR "\\n") as links from ${MYSQL_PREFIX}podlove_modules_shownotes_entry se, ${MYSQL_PREFIX}podlove_episode sep where sep.id=se.episode_id group by sep.id) se on se.id=ep.id 
         left join (select tr.object_id, group_concat(t.name SEPARATOR ", ") as kw from ${MYSQL_PREFIX}terms t, ${MYSQL_PREFIX}term_relationships tr, ${MYSQL_PREFIX}term_taxonomy tx where t.term_id=tx.term_id and tr.term_taxonomy_id=tx.term_taxonomy_id group by tr.object_id) kw on kw.object_id=po.id 
-        where ep.number is not NULL and po.post_status not in ("auto-draft","inherit")
+        where ep.number is not NULL and po.post_status not in ("auto-draft","inherit","trash")
 EOM
 #        left join (select pm.post_id, pt.guid from ${MYSQL_PREFIX}postmeta pm,${MYSQL_PREFIX}posts pt where pm.meta_key="_thumbnail_id" and pt.id=pm.meta_value) th on th.post_id=po.id where ep.number is not NULL and po.post_status not in ("auto-draft","inherit")
 }
@@ -33,8 +37,8 @@ function load_ep_data_SQL () {
     fi
     IFS=$'|' read -r _ EPISODE_SLUG EPISODE_TITLE EPISODE_REC_DATE EPISODE_POST_DATE EPISODE_KEYWORDS EPISODE_SUMMARY <<< \
         $( awk -F"\t" -r -v ep=${episode} '{if ($1==ep) {print gensub(/\t/,"|","g",$0)}}' episode_export.txt)
-    EPISODE_SUMMARY=$(awk -F"\t" -r -v ep=${episode} '{if ($1==ep) {val= gensub(/\r/,"","g",$7);val=gensub (/\\t/,"\t","g",val); val=gensub(/\\n/,"\n","g",val); print val}}' episode_export.txt|perl -Mopen=locale -pe 's/&#x([\da-f]+);/chr hex $1/gie')
-    EPISODE_LINKS=$(awk -F"\t" -r -v ep=${episode} '{if ($1==ep) {val= gensub(/\r/,"","g",$8);val=gensub (/\\t/,"\t","g",val); val=gensub(/\\n/,"\n","g",val); print val}}' episode_export.txt)
+    EPISODE_SUMMARY=$(awk -F"\t" -v ep=${episode} '{if ($1==ep) {print $7}}' episode_export.txt |sed -e 's/\r//g;s/\\n/\n/g;s/\\t/\t/g'|perl -Mopen=locale -pe 's/&#x([\da-f]+);/chr hex $1/gie')
+    EPISODE_LINKS=$(awk -F"\t" -v ep=${episode} '{if ($1==ep) {print $8}}' episode_export.txt |sed -e 's/\r//g;s/\\n/\n/g;s/\\t/\t/g')
 }
 
 function update_ep_image_SQL () {
@@ -50,9 +54,7 @@ EOM
 }
 
 function mktemp_jpg () {
-    local fn="$(mktemp)"
-    mv ${fn} ${fn}.jpg
-    echo "${fn}.jpg"
+    mktemp --suffix=.jpg
 }
 
 function get_overlay_logo() {
@@ -77,9 +79,15 @@ function get_temp_image() {
     local image="$1"
     local episode_tmp="$(mktemp)"
     curl -L "$image" >$episode_tmp 2>/dev/null
-    local ext=$(file /data/podcast/podcast/zartosztaly-new.jpg --extension|cut -d: -f2|cut -d/ -f1|tr -d " ")
-    mv "${episode_tmp}" "${episode_tmp}.${ext}"
-    echo "${episode_tmp}.${ext}"
+    local ext=$(file ${episode_tmp} --extension|cut -d: -f2|cut -d/ -f1|tr -d " ")
+
+    if [ ! "${ext}" = "???" ]; then
+	    mv "${episode_tmp}" "${episode_tmp}.${ext}"
+	    echo "${episode_tmp}.${ext}"
+    else
+	    rm "${episode_tmp}" 
+    	    echo ""
+    fi
 }
 
 function get_overlay_image() {
@@ -150,7 +158,7 @@ function create_chapter_frame() {
     echo "Creating chapter frame title: ${text} image: ${image}" >&2
     local frame="$(mktemp_jpg)"
     local angle=$(( $RANDOM %30 - 15 ))
-    if [ -z "${image}" ]; then
+    if [ -z "${image}" ] && [ ! -s "${image}" ]; then
         local xoff=$(( $RANDOM % 360 - 180 ));  [ -z $(echo $xoff|grep '-') ] && xoff='+'$xoff
         local yoff=$(( $RANDOM % 780 - 450 )); [ -z $(echo $yoff|grep '-') ] && yoff='+'$yoff
 	convert -background khaki -border 1 -bordercolor gray -size 720x210 \
@@ -189,7 +197,7 @@ function add_episode_frame() {
     echo "Creating frame in ${script} with duration ${duration} title: ${text} image: ${image}" >&2
     local frame="$(mktemp_jpg)"
     local angle=$(( $RANDOM % 30 - 15 ))
-    if [ -z "${image}" ]; then
+    if [ -z "${image}" ] || [ ! -s "${image}" ]; then
         local xoff=$(( $RANDOM % 160 - 80 ));  [ -z $(echo $xoff|grep '-') ] && xoff='+'$xoff
         local yoff=$(( $RANDOM % 300 - 150 )); [ -z $(echo $yoff|grep '-') ] && yoff='+'$yoff
 	convert -background khaki -border 1 -bordercolor gray -size 450x150 \
@@ -220,26 +228,27 @@ function create_images () {
     local skip_image_frames=$2
     local last_chapter_frame="${TARGET_DIR}/${EPISODE_SLUG}_cover.jpg"
     local position=0
-    local last_position=2
+    local last_position=0
     local pd="$(echo $EPISODE_POST_DATE|cut -d'T' -f1)"
 
     mkdir -p "${TARGET_DIR}/images/${EPISODE_SLUG}/"
 
     #Create video background frame and title overlay
     if [ ! "${skip_video_frames}" = "true" ]; then
-	SCRIPT=$(mktemp)
+	SCRIPT=$(mktemp --suffix=.frames)
+	CHAPTER_METADATA=$(mktemp --suffix=.meta)
+
 	echo "Adding title frame in ${SCRIPT} title: ${text}" >&2
     	local overlay_frame=$(get_overlay_image)
       	local title_frame=$(create_title_frame "$overlay_frame" "$PODCAST_TITLE\n$EPISODE - $EPISODE_TITLE\n(${pd})")
       	cp ${title_frame} "${TARGET_DIR}/${EPISODE_SLUG}_title.jpg"
 	local last_video_frame="$overlay_frame"
-	echo "ffconcat version 1.0
-
-file ${title_frame}" > ${SCRIPT}
+	echo -e "ffconcat version 1.0\n\nfile ${title_frame}\n" > ${SCRIPT}
+	echo -e ";FFMETADATA1\ntitle=$(escape_ffmpeg_metadata "${EPISODE_TITLE}")\ndecription=$(escape_ffmpeg_metadata "${EPISODE_SUMMARY}")\nshow=$(escape_ffmpeg_metadata "${PODCAST_TITLE}")\nepisode_id=$EPISODE\n\n" > ${CHAPTER_METADATA}
     fi
 
-    local psc_in="${SRC_DIR}/${EPISODE_SLUG}.psc"
-    local psc_out="${TARGET_DIR}/${EPISODE_SLUG}.psc"
+	    local psc_in="${SRC_DIR}/${EPISODE_SLUG}.psc"
+	    local psc_out="${TARGET_DIR}/${EPISODE_SLUG}.psc"
     local last_title=""
     echo '<psc:chapters xmlns:psc="http://podlove.org/simple-chapters" version="1.2">' >"${psc_out}"
     while IFS= read -r line || [ -z "${done_last}" ] ; do
@@ -255,6 +264,9 @@ file ${title_frame}" > ${SCRIPT}
 	    "start" )
 		local position_raw=$v
 		local position="$(expr $(echo $v|cut -c 1-2) \* 3600 + $(echo $v|cut -c 4-5) \* 60 + $(echo $v|cut -c 7-8))"
+		if [ ! -z "$prev_title" ]; then
+  		    echo -e "[CHAPTER]\nTIMEBASE=1/1000\nSTART=${last_position}000\nEND=${position}000\ntitle=$(escape_ffmpeg_metadata "${prev_title}")\n" >>$CHAPTER_METADATA
+		fi
 		;;
 	    "image" )
 		local image="$(echo $v |perl -C -MHTML::Entities -pe 'decode_entities($_);')"
@@ -265,8 +277,11 @@ file ${title_frame}" > ${SCRIPT}
 	esac
 	if ( [ "$k" = "title" ] && [ "${last_title}" != "${v}" ] ) || [ ! -z "${done_last}" ]; then
 		    local duration=$(( $position - $last_position ))
+    		    if [ ! -z "${image}" ]; then
+		    	local temp_image=$(get_temp_image "${image}")
+		    fi
 		    if [ ! "${skip_image_frames}" = "true" ]; then 
-		    	mv $(create_chapter_frame "${last_chapter_frame}" "${last_title}" "${image}") "${TARGET_DIR}/images/${EPISODE_SLUG}/${EPISODE_SLUG}_${position}.jpg"
+		    	mv $(create_chapter_frame "${last_chapter_frame}" "${last_title}" "${temp_image}") "${TARGET_DIR}/images/${EPISODE_SLUG}/${EPISODE_SLUG}_${position}.jpg"
 		    	local last_chapter_frame="${TARGET_DIR}/images/${EPISODE_SLUG}/${EPISODE_SLUG}_${position}.jpg"
 		    fi
 		    echo "	<psc:chapter title='${last_title}' start='${position_raw}' " \
@@ -274,11 +289,13 @@ file ${title_frame}" > ${SCRIPT}
 			$([ ! -z "$href" ] && echo "href='$(echo $href|perl -C -MHTML::Entities -pe 'encode_entities($_);')'") \
 			"/>" >> "${psc_out}"
 		    if [ ! "${skip_video_frames}" = "true" ]; then 
-			    last_video_frame=$(add_episode_frame "$SCRIPT" "$duration" "$last_video_frame" "$last_title" "$image")
+		    	last_video_frame=$(add_episode_frame "$SCRIPT" "$duration" "$last_video_frame" "$last_title" "$temp_image")
 		    fi
+		    [ ! -z "${temp_image}" ] && rm "${temp_image}"
 		    local last_position=$position
 		    local href=""
 		    local image=""
+		    local prev_title="$last_title"
 		    local last_title="$(echo $v |perl -C -MHTML::Entities -pe 'decode_entities($_);')"
 	fi
     done <<< $( xpath -q -e '//@title|//@start|//@image|//@href' "${psc_in}" )
@@ -293,6 +310,9 @@ file ${title_frame}" > ${SCRIPT}
 	local duration=$(( $position - $last_position ))
 	echo "duration ${duration}.00" >>$SCRIPT
 	echo "file ${last_video_frame}" >>$SCRIPT
+	if [ ! -z "$prev_title" ]; then
+  	    echo -e "[CHAPTER]\nTIMEBASE=1/1000\nSTART=${last_position}000\nEND=${position}000\ntitle=$(escape_ffmpeg_metadata "${prev_title}")\n" >>$CHAPTER_METADATA
+	fi
 	# Remove overlay if there are other frames in the video
 	[ "${overlay_frame}" == "${last_video_frame}" ] ||  rm "$overlay_frame"
     fi
@@ -300,14 +320,27 @@ file ${title_frame}" > ${SCRIPT}
 
 function create_video () {
     local sequence="$1"
+    local chapters="$2"
+
     local mp3src="${TARGET_DIR}/${EPISODE_SLUG}.mp3"
-    local src_params="-hide_banner -y -f concat -safe 0 -i ${sequence}"
-    local video_out_params="-c:v h264 -preset fast -tune stillimage -profile:v high -pix_fmt yuvj420p -fps_mode vfr -vf fps=20 -b:v 3500K -async 1"
+    local ff_params="-hide_banner -y -hwaccel vaapi" 
+    local video_params="-c:v h264 -preset fast -tune stillimage -profile:v high -pix_fmt yuvj420p -async 1 -f mp4"
+    local video_out_params="-fps_mode vfr -vf fps=24 -b:v 500K"
+#    local video_out_params="-crf 18"
     local audio_out_params="-ac 2 -c:a aac -b:a 192K"
+
+    local tmpvid=$(mktemp --suffix=.mp4)
+    set -x
+    ffmpeg ${ff_params} -f concat -safe 0 -i ${sequence} ${video_params} -r 1  ${tmpvid}
 #    local audio_out_params="-ac 2 -c:a copy"
-    ffmpeg ${src_params} ${video_out_params} -pass 1 -an -f mp4 /dev/null && \
-	ffmpeg ${src_params} -i ${mp3src} -shortest ${video_out_params} -pass 2 ${audio_out_params} "${TARGET_DIR}/${EPISODE_SLUG}.mp4"
-#    ffmpeg ${src_params} -i ${mp3src} -shortest ${video_out_params} ${audio_out_params} "${TARGET_DIR}/${EPISODE_SLUG}.mp4"
+#    ffmpeg ${ff_params} -i ${tmpvid} ${video_params} ${video_out_params} -pass 1 -an /dev/null && \
+#        ffmpeg ${ff_params} -i ${tmpvid} -i ${mp3src} -shortest ${video_params} ${video_out_params} -pass 2 ${audio_out_params} "${TARGET_DIR}/${EPISODE_SLUG}.mp4"
+#    rm ffmpeg2pass-0.log ffmpeg2pass-0.log.mbtree
+    ffmpeg ${src_params} -i ${tmpvid} -i ${chapters} -i ${mp3src} -shortest \
+	    ${video_params} ${video_out_params} \
+	    -map_metadata 1 -map_chapters 1 \
+	    ${audio_out_params} "${TARGET_DIR}/${EPISODE_SLUG}.mp4"
+    rm $(tmpvid)
 }
 
 function remove_images () {
@@ -367,7 +400,6 @@ if [ -z "${EPISODE}" ]; then
 	usage
 	exit 1
 fi
-
 [ -e "${TARGET_DIR}" ] || mkdir -p "${TARGET_DIR}"
 [ -e "${TARGET_DIR}/images" ] || mkdir -p "${TARGET_DIR}/images"
 
@@ -417,9 +449,10 @@ if [ ! -e "${TARGET_DIR}/${EPISODE_SLUG}.mp4" ] || [ ! -d "${TARGET_DIR}/images/
 fi
 if [ ! "${IMAGEONLY}" = "true" ] && [ ! -e "${TARGET_DIR}/${EPISODE_SLUG}.mp4" ]; then
   if [ -e "$SCRIPT" ]; then
-      create_video $SCRIPT
+      create_video $SCRIPT $CHAPTER_METADATA
       tree -H "/podcast" -L 1 --noreport --charset utf-8 -P "*.mp?" -o ${TARGET_DIR}/index.html ${TARGET_DIR}
       remove_images "${SCRIPT}"
+      rm ${CHAPTER_METADATA}      
   fi
 fi
 
